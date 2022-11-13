d235c3535b5db60f03c6f394e6ed5333eee506fe
Nishanth Salinamakki <nishanthrs@meta.com>
2022-11-13 01:23:36 -0500

    Added MDX extension and finished about page

--- next.config.js
+++ next.config.js
@@ -3,5 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 }
+// Ref: https://www.npmjs.com/package/@next/mdx
+const withMDX = require('@next/mdx')()
 
 module.exports = nextConfig
+module.exports = withMDX()
